ab3aa9c3a89789ed9dba25d5578c95ed8d84089d
fengde <jun1st.feng@gmail.com>
2019-10-18 11:52:06 +0800

    Initial Commit

---
+++ my socket/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  my socket
+//
+//  Created by fengde on 2019/10/18.
+//  Copyright © 2019 fengde. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
